1d5551eddf7fc00b20757157ff406b2838ce0ce0
apurvpersonal <apurvalion@gmail.com>
2024-01-23 01:02:56 +0530

    Moved the code Block Into a separate component

---
+++ src/Pages/PuzzleScreen/Components/CodeBlock/CodeBlockComponent.tsx
@@ -0,0 +1,51 @@
+import {PuzzleList} from "../../../../assets/Text/PuzzleList.ts";
+import {Dispatch, SetStateAction, useEffect, useState} from "react";
+import {Box, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import {CodeBlock, dracula} from "react-code-blocks";
+
+
+type CodeBlockComponentProps={
+    setPuzzleOptions:Dispatch<SetStateAction<string[]>>
+}
+
+const CodeBlockComponent=(props:CodeBlockComponentProps)=>{
+    const {setPuzzleOptions} = props;
+    const randomKey = Object.keys(PuzzleList)[Math.floor(Math.random() * Object.keys(PuzzleList).length)]
+    const [selectedLanguage,setSelectedLanguage] = useState(randomKey);
+    const randomPuzzle = Math.floor(Math.random() * PuzzleList[selectedLanguage].length)
+    const [puzzle,setPuzzle] = useState(PuzzleList?.[selectedLanguage]?.[randomPuzzle]??PuzzleList[selectedLanguage][0])
+    useEffect(() => {
+        setPuzzle(PuzzleList?.[selectedLanguage]?.[randomPuzzle]??PuzzleList[selectedLanguage][0])
+        setPuzzleOptions(puzzle.Options??[])
+    }, [randomPuzzle,selectedLanguage]);
+    return(
+        <Box display={{xs:'flex'}} flexDirection={{xs:'column'}} alignItems={{xs:'center'}}  sx={{width: {xs: '90%', lg: '20rem'}}}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: '100%' }}>
+            <InputLabel id="language-selector">Select A language</InputLabel>
+            <Select
+
+                labelId="demo-simple-select-label"
+                id="language-selector"
+                sx={{color:'black',marginBottom:'1rem',width:'100%',borderRadius:'5rem'}}
+                value={selectedLanguage}
+                onChange={(e)=>setSelectedLanguage(e.target.value)}
+            >
+                {Object.keys(PuzzleList).map((language,index) => (
+                    <MenuItem sx={{color:'black'}} key={index} value={language}>{language}</MenuItem>
+                ))}
+            </Select>
+            </FormControl>
+            <CodeBlock
+                text={puzzle.Code}
+                codeContainerStyle={{width: '100%',borderRadius:'5rem'}}
+                language={selectedLanguage}
+                startingLineNumber={1}
+                showLineNumbers={false}
+                theme={dracula}
+            />
+        </Box>
+    )
+
+}
+
+export default CodeBlockComponent;
--- src/Pages/PuzzleScreen/PuzzleScreen.tsx
+++ src/Pages/PuzzleScreen/PuzzleScreen.tsx
@@ -1,48 +1,33 @@
 import styles from './PuzzleScreen.module.css'
 import {Box, Container, Divider, FormControlLabel, Paper, Radio, RadioGroup, Typography} from "@mui/material";
 import Greetings from '../../assets/Svg/greetings.svg'
-import {CodeBlock, dracula} from "react-code-blocks";
-import {PuzzleList} from "../../assets/Text/PuzzleList.ts";
-import {useEffect, useState} from "react";
+import {useState} from "react";
+import CodeBlockComponent from "./Components/CodeBlock/CodeBlockComponent.tsx";
 
 const PuzzleScreen = () => {
-    // @todo: Add a way to select a puzzle from the list and Convert the code block part into a separate component
-    const randomKey = Object.keys(PuzzleList)[Math.floor(Math.random() * Object.keys(PuzzleList).length)]
-    const randomPuzzle = Math.floor(Math.random() * PuzzleList[randomKey].length)
-    const [puzzle,setPuzzle] = useState(PuzzleList?.[randomKey]?.[randomPuzzle]??PuzzleList[randomKey][0])
-    useEffect(() => {
-        setPuzzle(PuzzleList?.[randomKey]?.[randomPuzzle]??PuzzleList[randomKey][0])
-    }, [randomPuzzle,randomKey]);
+    const [puzzleOptions,setPuzzleOptions] = useState<string[]>([]);
     return (
         <Container sx={{display: 'flex'}} maxWidth={'xl'} className={styles.PuzzleWrapper}>
-            <Paper sx={{padding: 2, borderRadius: 4, position: 'relative', width: {sx: '70%', lg: '50%'}}}
+            <Paper sx={{paddingX: 2,paddingY:5, borderRadius: 4, position: 'relative', width: {sm: '70%', lg: '50%'}}}
                    className={styles.PuzzleContainer} square={false} elevation={10}>
-                <Typography textAlign={'center'} color={'black'} variant={'h4'} fontWeight={600}>Brace yourself! This place is like a VIP club. Are you on the guest list or just hoping for a virtual bouncer to give you the nod?</Typography>
-                <Box marginTop={5} justifyContent={'space-evenly'} display={'flex'} gap={5} alignItems={'center'}>
-                    <Box maxWidth={'xl'} marginLeft={5}>
-                        <Typography textAlign={'center'} marginBottom={2} color={'black'} variant={'h6'} fontWeight={600}>{randomKey}</Typography>
-                        <CodeBlock
-                            codeBlockStyle={{width: '100%',borderRadius:'5rem'}}
-                            text={puzzle.Code}
-                            codeContainerStyle={{width: '100%',borderRadius:'5rem'}}
-                            language={randomKey}
-                            startingLineNumber={1}
-                            showLineNumbers={true}
-                            theme={dracula}
-                        />
+                <Typography textAlign={'center'} color={'black'} variant={'h4'} fontSize={{sx:'1rem',lg:'2rem'}} fontWeight={600}>Brace yourself! This place is like a VIP club. Are you on the guest list or just hoping for a virtual bouncer to give you the nod?</Typography>
+                <Box height={{xs:250,md:'auto'}} marginTop={5} overflow={'auto'} justifyContent={'space-evenly'} display={'flex'} gap={5} alignItems={'center'} flexDirection={{xs:'column',md:'row',lg:'row'}}>
+                    <Box marginTop={{xs:45, md:0}} maxWidth={'xl'} marginLeft={{md:6}}>
+                        <CodeBlockComponent setPuzzleOptions={setPuzzleOptions}/>
                     </Box>
                     <Divider orientation={'vertical'} flexItem={true} variant={'fullWidth'} color={'primary'} />
                     <Box>
+                        <Typography textAlign={'center'} marginBottom={3} color={'black'} variant={'h5'} fontWeight={400}>Select the correct output</Typography>
                         <RadioGroup
                             name="radio-buttons-group"
                         >
-                            {puzzle.Options.map((option,index) => (
+                            {puzzleOptions.map((option,index) => (
                                 <FormControlLabel key={index} value={option} control={<Radio />} label={option} labelPlacement={'end'} sx={{color:'black'}} />
                             ))}
                         </RadioGroup>
                     </Box>
                 </Box>
-                <img src={Greetings} className={styles.GreetingsImage}/>
+                <img src={Greetings} className={styles.GreetingsImage} alt={'bannerImg'}/>
             </Paper>
         </Container>
     )
