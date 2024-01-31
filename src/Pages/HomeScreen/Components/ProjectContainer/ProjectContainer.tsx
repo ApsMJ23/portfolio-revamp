4e3ceadea8e1fef227e1863e84472861abd81d3a
apurvpersonal <apurvalion@gmail.com>
2024-02-01 00:51:55 +0530

    Designed a classy Project Section

--- src/Pages/HomeScreen/Components/ProjectContainer/ProjectContainer.tsx
+++ src/Pages/HomeScreen/Components/ProjectContainer/ProjectContainer.tsx
@@ -1,29 +1,35 @@
-import {Box, Card, CardContent, CardHeader, Container, Typography} from "@mui/material";
+import {Box, Card, Container, Typography} from "@mui/material";
 import styles from './ProjectContainer.module.css';
+import {useAutoAnimate} from "@formkit/auto-animate/react";
+import {useState} from "react";
 
 
 const ProjectContainer = () => {
+    const [animateParent] = useAutoAnimate()
+    const [showDescription,setShowDescription] = useState('');
     return (
-        <div className={styles.ProjectContainer}>
+        <Container maxWidth={'xl'} sx={{borderRadius:{xs:10,md:'20rem 0 0 20rem'}, marginTop:20,marginLeft:{xs:0,md:10},paddingX:5,paddingY:5,height:700}} className={styles.ProjectContainer}>
             <Container maxWidth={'xl'}>
-                <Box display={'flex'} flexDirection={'column'} width={'100%'} marginTop={20} justifyContent={'center'}
+                <Box display={'flex'} flexDirection={'column'} width={'100%'}  justifyContent={'center'}
                      alignItems={'center'}>
                     <Typography sx={{ fontFamily: "'Fjalla One', sans-serif",textDecoration:'underline'}} variant={'h1'}>Projects</Typography>
                     <Typography sx={{fontSize:20}} marginTop={2} textAlign={'center'} variant={'h3'}>Some of my projects</Typography>
                 </Box>
-                <Box marginTop={10} display={'flex'} alignItems={'center'} gap={5}>
-                    <Card square={false} variant={'elevation'} elevation={10} sx={{width: {xs: '100%', md: '30%'}, height: {xs: '100%', md: '100%'},background:'#444'}}>
-                        <CardHeader title={'Airbnb Clone'} titleTypographyProps={{variant:'h3',fontWeight:600,textAlign:'center'}} sx={{background:'#6e5494'}}/>
-                        <img src="https://cdn.freebiesupply.com/images/large/2x/airbnb-logo.png" alt="airbnb" style={{width:'100%',height:'100%'}}/>
-                        <CardContent>
-                            <Typography variant="body2" color="text.secondary">
-                                This is a project
-                            </Typography>
-                        </CardContent>
+                <Box marginLeft={{xs:0,md:20}} overflow={'auto'} marginTop={10} display={'flex'} flexWrap={'wrap'} alignItems={'center'} width={{md:'80%',xs:'100%'}} gap={5}>
+                    <Card onMouseEnter={()=>setShowDescription('hello')} onMouseLeave={()=>setShowDescription('')} ref={animateParent} sx={{width:{xs:'100%',md:300},height:300,background:'#292929',display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}} variant={'elevation'} elevation={10} >
+                        <Typography sx={{color:'beige'}} variant={'h3'}>Project 1</Typography>
+                        {showDescription==='hello'&&<Typography sx={{color:'beige',whiteSpace:'nowrap'}} variant={'h4'}>Project 1 Description</Typography>}
                     </Card>
+                    <Box width={600}>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci alias
+                            asperiores atque autem consequatur consequuntur cumque cupiditate, deserunt dicta dolor
+                            doloremque doloribus dolorum eaque earum eligendi eos error esse est exercitationem
+                        </Typography>
+                    </Box>
                 </Box>
             </Container>
-        </div>
+        </Container>
     );
 }
 
