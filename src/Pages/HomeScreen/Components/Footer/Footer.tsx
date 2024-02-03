ae78adf9f9ccf66aa4f879f31e3986a86ef35e6b
Apurva Singh <apurvalion@gmail.com>
2024-02-04 00:57:07 +0530

    Added Comments

--- src/Pages/HomeScreen/Components/Footer/Footer.tsx
+++ src/Pages/HomeScreen/Components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import moment from "moment";
 
 
 const Footer = () => {
+    // This component is the footer of the home screen
     return (
         <Paper elevation={15} sx={{background:'#333',position:'relative',zIndex:10}}>
             <div style={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', padding:20}}>
