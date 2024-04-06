7df8bffc50837ffd7bb42e5e7cf9aaaa03d530b9
Apurva Singh <apurvalion@gmail.com>
2024-04-06 17:30:19 +0530

    Completed Project Section

--- src/Pages/HomeScreen/Components/ProjectContainer/ProjectContainer.tsx
+++ src/Pages/HomeScreen/Components/ProjectContainer/ProjectContainer.tsx
@@ -5,9 +5,9 @@ import VerticalCarousel from "./Components/VerticalCarousel/VerticalCarousel.tsx
 
 const ProjectContainer = () => {
     return (
-        <Container maxWidth={'xl'} sx={{marginTop:20,paddingX:5,paddingY:5,display:'flex',gap:10}} className={styles.ProjectContainer}>
+        <Container maxWidth={'xl'} sx={{marginTop:20,paddingX:5,paddingY:5,display:'flex',gap:{md:10,xs:0},flexWrap:{xs:'wrap',md:'nowrap'}}} className={styles.ProjectContainer}>
             <Box position={'sticky'} top={0} display={'flex'} justifyContent={'center'} alignItems={'center'} width={{md:'50%',xs:'100%'}}>
-                <Typography variant={'h1'} fontSize={{md:150,xs:40}} fontWeight={600} fontFamily={"'Fjalla One',sans-serif"}>Projects For Everyday</Typography>
+                <Typography variant={'h1'} fontSize={{md:150,xs:40}} fontWeight={600} fontFamily={"'Fjalla One',sans-serif"}>Freelance Projects</Typography>
             </Box>
             <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} width={{md:'50%',xs:'100%'}}>
                 <VerticalCarousel/>
